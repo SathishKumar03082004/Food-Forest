cde5a4a4cc151d94cb2743b27838e42dbeb116a4
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2024-04-02 22:51:34 +0530

    File

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from fastapi import FastAPI,Request,Depends,Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse,JSONResponse
-from database import SessionLocal
-from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import Session
-import models
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi import Form
 
 app = FastAPI()
@@ -15,81 +14,47 @@ app.mount("/templates", StaticFiles(directory="templates"), name="templates")
 
 
 
-def get_db():
-    db=None
-    try:
-        db=SessionLocal()
-        yield db
-    finally:
-        db.close()
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-
-@app.get('/test')
-def get_test(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("test.html", {"request": request})
+app = FastAPI()
 
 
 
-@app.get('/home')
-def get_home(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("home.html", {"request": request})
-
-
-
-@app.get('/login')
-def get_login(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-@app.post("/logcheck")
-def logcheck(request:Request,db:Session=Depends(get_db),login_user:str=Form(...),login_password:str=Form(...)):
-    print(login_user,login_password)
-    find=db.query(models.SignUp).filter(models.SignUp.username==login_user,models.SignUp.password==login_password).first()
-    if find is None:
-        error= "Invalid Username or Password!"   
-        json_compatible_item_data = jsonable_encoder(error)
-        return JSONResponse(content=json_compatible_item_data)
-    else:
-        error= "Done"   
-        json_compatible_item_data = jsonable_encoder(error)
-        return JSONResponse(content=json_compatible_item_data)
-
-
-
-@app.get('/dashboard')
-def get_dashboard(request: Request, db: Session = Depends(get_db)):
+@app.get('/', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
 
 
-@app.get('/order')
-def get_order(request: Request, db: Session = Depends(get_db)):
+@app.get('/order', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("order.html", {"request": request})
 
 
 
-@app.get('/explore')
-def get_explore(request: Request, db: Session = Depends(get_db)):
+@app.get('/explore', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("explore.html", {"request": request})
 
 
 
-@app.get('/hotal')
-def get_hotal(request: Request, db: Session = Depends(get_db)):
+@app.get('/hotal', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("hotal.html", {"request": request})
 
 
-@app.get('/cart')
-def get_cart(request: Request, db: Session = Depends(get_db)):
+@app.get('/cart', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("cart.html", {"request": request})
 
 
 
-@app.get('/profile')
-def get_profile(request: Request, db: Session = Depends(get_db)):
+@app.get('/profile', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("profile.html", {"request": request})
 
 
-@app.get('/payment')
-def get_payment(request: Request, db: Session = Depends(get_db)):
+@app.get('/payment', response_class=HTMLResponse)
+async def get_order(request: Request):
     return templates.TemplateResponse("payment.html", {"request": request})
